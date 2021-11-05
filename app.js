8ded7a512751e92afe3567c3c45b2a58b8b8e05b
AmanPant777 <footballeramanpt65@gmail.com>
2021-11-05 20:24:55 +0530

    8. add rootquer to query book with id add schema to app

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express=require('express')
-const graphqlHTTP=require('express-graphql')
-
+const {graphqlHTTP}=require('express-graphql')
+const Schema=require('./schema/schema.js')
 
 
 const app=express()
 app.use('/graphql',graphqlHTTP({
-    
-}))
+ schema:Schema
+})
+)
 
 
 
