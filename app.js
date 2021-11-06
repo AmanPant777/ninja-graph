989dcc5b40df83ee8238688fc8e00f8db4eb458b
AmanPant777 <footballeramanpt65@gmail.com>
2021-11-06 14:41:12 +0530

    add resolve and test in browser graphql

--- app.js
+++ app.js
@@ -5,15 +5,9 @@ const Schema=require('./schema/schema.js')
 
 const app=express()
 app.use('/graphql',graphqlHTTP({
- schema:Schema
+ schema:Schema,
+ graphiql:true
 })
 )
-
-
-
-
-
-
-
 
 app.listen(4000,()=>console.log('App listening to Port 4000'))
